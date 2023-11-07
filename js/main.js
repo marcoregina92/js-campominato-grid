d99a2306638be6f92ceb0a162c03e97dae1d1e1c
Marco <marcoregina92@gmail.com>
2023-11-07 20:26:19 +0100

    aggiunge button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 let numCelle = 100;
 let grid = document.getElementById("grid");
+let btnPlay = document.getElementById("btnPlay");
+let level = document.getElementById("level");
 
-for (let i = 1; i <= numCelle; i++) {
-    const cella = document.createElement("div");
-    cella.classList.add("square");
-    cella.innerHTML = i;
-    grid.appendChild(cella); 
+
+
+btnPlay.addEventListener("click", function() {
+
+    for (let i = 1; i <= numCelle; i++) {
+        const cella = document.createElement("div");
+        cella.classList.add("square");
+        grid.appendChild(cella); 
+        
+        cella.addEventListener("click", function(){
     
-    cella.addEventListener("click", function(){
-
-        console.log("cella selezionata", i);
-        cella.classList.toggle("highlight")
-
-    })
-};
+            console.log("cella selezionata", i);
+            cella.innerHTML = i;
+            cella.classList.toggle("highlight");
+    
+        });
+    };
+});
