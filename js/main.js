4da6e838d1cba00f927dcd363c1b78c3d74e1078
Marco <marcoregina92@gmail.com>
2023-11-08 17:45:23 +0100

    aggiunge numeri casuali

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+//***** Definisco le costanti globali che userò ****
+
 let numCelle = 0;
 let grid = document.getElementById("grid");
 let btnPlay = document.getElementById("btnPlay");
 
+// **** Imposto il livello di difficoltà da scegliere ****
+
 let level = document.getElementById("level");
-level.addEventListener("click", function () {
+level.addEventListener("change", function () {
     console.log("Hai selezionato: ", level.value);
 });
 
-
-
+//**** Aggiungo l'evento quando si clicca sul button e in base alla difficoltà cambio il numero di celle da impostare ***
 btnPlay.addEventListener("click", function () {
     if (level.value == "medium") {
         numCelle = 81;
@@ -23,8 +26,20 @@ btnPlay.addEventListener("click", function () {
     console.log(numCelle);
     console.log(level);
 
+    let celleBomba = [];
+    while (celleBomba.length < 16) {
+        let random = randomNumber(1, numCelle);
+        if(celleBomba.indexOf(random) === -1) {
+        celleBomba.push(random);
+       }
+    }
+    console.log(`Celle bomba: ${celleBomba}`);
+
+
+
+
     for (let i = 1; i <= numCelle; i++) {
-        let cella = newCell(i);
+        let cella = newCell(i, celleBomba);
         grid.appendChild(cella);
         grid.appendChild(cella);
         if (numCelle == 81) {
@@ -49,3 +64,7 @@ function newCell(numero) {
 
     return cella;
 };
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
